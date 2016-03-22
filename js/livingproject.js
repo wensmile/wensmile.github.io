07d94be5349767bb1d11dbb5c85d5b1bdc0a4bcd
wensmile <smiley84.lin@gmail.com>
2016-03-22 19:13:16 +0800

    google change

--- js/livingproject.js
+++ js/livingproject.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-	var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    	center: {lat: 23.667893, lng: 120.951698},
-        zoom: 8
-    });
 	$("#btnTainan").click(function () {
     $("#tainan").show();
     $("#front").hide();
