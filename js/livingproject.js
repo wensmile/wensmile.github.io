266141587c5e3a9374ea13d4d6396c6c3d03112f
wensmile <smiley84.lin@gmail.com>
2016-03-22 22:22:06 +0800

    map change

--- js/livingproject.js
+++ js/livingproject.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
+
 	var map = new google.maps.Map($('#map-canvas')[0], {
 		center: {lat: 23.718189, lng: 121.006771},
 		zoom: 10,
 	});
 	//map.setCenter(new google.maps.LatLng(22.997722, 120.212387));
 	var marker = new google.maps.Marker({
-		position: {lat: -34.397, lng: 150.644},
+		position: {lat: 23.718189, lng: 121.006771},
 		map: map,
 		title:"Taiwan!"
+	});
+	var markerTainan = new google.maps.Marker({
+		position: {lat: 22.997722, lng: 120.212387},
+		map: map,
+		title:"Tainan!"
 	});
 	$("#map-canvas").css('height', $(window).height()-150); 
 	$("#btnTainan").click(function () {
@@ -15,8 +21,8 @@ $(document).ready(function(){
 		$("#front").hide();
 		$("#lukang").hide();
 		$("#dulan").hide();
-		map.setCenter(new google.maps.LatLng(22.997722, 120.212387));
 		map.setZoom(1);
+		map.setCenter(new google.maps.LatLng(22.997722, 120.212387));
 	});
 	$("#btnLukang").click(function () {
 		$("#lukang").show();
