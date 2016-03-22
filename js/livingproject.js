6d4ac322ed2f16fd0c3b4e899927ad1c982e6d72
wensmile <smiley84.lin@gmail.com>
2016-03-22 22:10:59 +0800

    google change

--- js/livingproject.js
+++ js/livingproject.js
@@ -1,6 +1,34 @@
 $(document).ready(function(){
+	var map = new google.maps.Map($('#map-canvas')[0], {
+		center: {lat: 23.718189, lng: 121.006771},
+		zoom: 8
+	});
+	//map.setCenter(new google.maps.LatLng(22.997722, 120.212387));
+	var marker = new google.maps.Marker({
+		position: {lat: -34.397, lng: 150.644},
+		map: map,
+		title:"Taiwan!"
+	});
+	$("#map-canvas").css('height', $(window).height()-150); 
 	$("#btnTainan").click(function () {
-    $("#tainan").show();
-    $("#front").hide();
-  });
+		$("#tainan").show();
+		$("#front").hide();
+		$("#lukang").hide();
+		$("#dulan").hide();
+		map.setCenter(new google.maps.LatLng(22.997722, 120.212387));
+		//map.setCenter(lat: 22.997722, lng: 120.212387);
+		//map.setZoom(3);
+	});
+	$("#btnLukang").click(function () {
+		$("#lukang").show();
+		$("#front").hide();
+		$("#tainan").hide();
+		$("#dulan").hide();
+	});
+	$("#btnDulan").click(function () {
+		$("#dulan").show();
+		$("#front").hide();
+		$("#lukang").hide();
+		$("#tainan").hide();
+	});	
 });
