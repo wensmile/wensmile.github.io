afee1fcba201b4eac6d35c8bb1a22bc4a20b4224
wensmile <smiley84.lin@gmail.com>
2016-03-22 23:07:41 +0800

    map change

--- js/livingproject.js
+++ js/livingproject.js
@@ -1,34 +1,45 @@
 $(document).ready(function(){
 	var taiwanC = new google.maps.LatLng(23.718189, 121.006771);
 	var tainanC = new google.maps.LatLng(22.997722, 120.212387);
+	var lukangC = new google.maps.LatLng(24.059338, 120.431415);
+	var dulanC = new google.maps.LatLng(22.871944, 121.226038);
 
 	var map = new google.maps.Map($('#map-canvas')[0], {
 		center: taiwanC,
-		zoom: 6,
-	});
-	//map.setCenter(new google.maps.LatLng(22.997722, 120.212387));
-	var marker = new google.maps.Marker({
-		position: taiwanC,
-		map: map,
-		title:"Taiwan!"
+		zoom: 7,
 	});
 	var infoTaiwan = new google.maps.InfoWindow();
   	infoTaiwan.setContent("TAIWAN!");
     infoTaiwan.setPosition(taiwanC);
     infoTaiwan.open(map);
-	var markerTainan = new google.maps.Marker({
+    var markerTainan = new google.maps.Marker({
 		position: tainanC,
-		map: map,
-		title:"Tainan!"
+		title:"Tainan",
 	});
+	var markerLukang = new google.maps.Marker({
+		position: lukangC,
+		title:"Lukang",
+	});
+	var markerDulan = new google.maps.Marker({
+		position: dulanC,
+		title:"Dulan",
+	});
+	function clearMarker() {
+		markerTainan.setMap(null);
+		markerLukang.setMap(null);
+		markerDulan.setMap(null);
+	}
+
 	$("#map-canvas").css('height', $(window).height()-150); 
 	$("#btnTainan").click(function () {
 		$("#tainan").show();
 		$("#front").hide();
 		$("#lukang").hide();
 		$("#dulan").hide();
+		clearMarker();
 		map.setZoom(14);
-		map.setCenter(new google.maps.LatLng(22.997722, 120.212387));
+		map.setCenter(tainanC);
+		markerTainan.setMap(map);
 	});
 	$("#btnLukang").click(function () {
 		$("#lukang").show();
